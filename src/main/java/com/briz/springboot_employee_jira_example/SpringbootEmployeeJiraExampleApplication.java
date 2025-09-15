ba0e90737cf229e45ae79d5868d7d451395b4262
kanchan raju <kanchanraju2015@gmail.com>
2025-09-15 12:38:45 +0530

    first jira commit

---
+++ src/main/java/com/briz/springboot_employee_jira_example/SpringbootEmployeeJiraExampleApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_employee_jira_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeJiraExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeJiraExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briz/springboot_employee_jira_example/SpringbootEmployeeJiraExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_employee_jira_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmployeeJiraExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
